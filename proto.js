cbf1468a7e6a13f0c0e3c00f9c73108dbe39bf07
dcooke28 <volex28@gmail.com>
2026-02-09 13:24:59 -0500

    Add more data

--- proto.js
+++ proto.js
@@ -3,11 +3,25 @@ const dataUrl = 'https://dcooke28.github.io/tbots/proto.json';
 const pageTitle = document.getElementById('pageTitle');
 const pageDescription = document.getElementById('pageDescription');
 
+const itemSiteNumber = document.getElementById('siteNumber');
+const itemTransect = document.getElementById('transect');
+const itemShovelTestNumber = document.getElementById('shovelTestNumber');
+const itemUtm = document.getElementById('utm');
+const itemStratumBaseDepth = document.getElementById('stratumBaseDepth');
+const photo = document.getElementById('photo');
+
 function displayItemPage(data, id) {
   const item = data.find(i => i.id == id);
 
   pageTitle.textContent = item.name;
-  pageDescription.textContent = item.description;
+  pageDescription.textContent = item.details;
+
+  itemSiteNumber.textContent = item.site_number;
+  itemTransect.textContent = item.transect;
+  itemShovelTestNumber.textContent = item.shovel_test_number;
+  itemUtm.textContent = item.utm;
+  itemStratumBaseDepth.textContent = item.stratum_base_depth;
+  photo.textContent = item.photo;
 }
 
 function getItemIdFromQuery() {
